6297d0316c400b5916d8425f740e1da67a464666
Gakawarstone <pinnekerv@gmail.com>
2023-08-29 12:13:17 +0200

    fix: add requiremets.txt to setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
+import os
 from setuptools import setup, find_packages
+
+lib_folder = os.path.dirname(os.path.realpath(__file__))
+requirement_path = f"{lib_folder}/requirements.txt"
+install_requires = []
+if os.path.isfile(requirement_path):
+    with open(requirement_path) as f:
+        install_requires = f.read().splitlines()
 
 setup(
     name="tgsend",
-    version="0.1",
+    version="0.2",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
             "tgsend = tgsend.main:main",
         ],
     },
+    install_requires=install_requires,
 )
