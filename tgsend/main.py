2e041b9b119b5640adfd305fc97e7d77cd8a4974
Gakawarstone <pinnekerv@gmail.com>
2025-03-23 19:20:09 +0100

    feat: move to uv

--- tgsend/main.py
+++ tgsend/main.py
@@ -9,37 +9,35 @@ from aiogram.types import FSInputFile
 
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-load_dotenv(ROOT_DIR + '/.env')
+load_dotenv(ROOT_DIR + "/.env")
 
 
 def get_config() -> Tuple[Bot, str]:
     try:
-        BOT_TOKEN = os.environ['BOT_TOKEN']
-        CHAT_ID = os.environ['CHAT_ID']
+        BOT_TOKEN = os.environ["BOT_TOKEN"]
+        CHAT_ID = os.environ["CHAT_ID"]
         return (Bot(BOT_TOKEN), CHAT_ID)
     except KeyError:
-        print('Run tgsend config')
+        print("Run tgsend config")
         exit(1)
 
 
 async def async_main():
-    if sys.argv[1] == 'config':
-        open(ROOT_DIR + '/.env', 'w').write(
-                'BOT_TOKEN=' + input('BOT_TOKEN=') + '\n')
-        open(ROOT_DIR + '/.env', 'a').write(
-                'CHAT_ID=' + input('CHAT_ID=') + '\n')
+    if sys.argv[1] == "config":
+        open(ROOT_DIR + "/.env", "w").write("BOT_TOKEN=" + input("BOT_TOKEN=") + "\n")
+        open(ROOT_DIR + "/.env", "a").write("CHAT_ID=" + input("CHAT_ID=") + "\n")
         return
 
     bot, chat_id = get_config()
 
     match sys.argv[1]:
-        case '-m':
-            text = ' '.join(sys.argv[2:])
+        case "-m":
+            text = " ".join(sys.argv[2:])
             await bot.send_message(chat_id, text)
         case _:
             bot, chat_id = get_config()
             for path in sys.argv[1:]:
-                file = FSInputFile(os.getcwd() + '/' + path)
+                file = FSInputFile(os.getcwd() + "/" + path)
                 await bot.send_document(chat_id, file)
 
     await bot.session.close()
@@ -49,5 +47,5 @@ def main():
     asyncio.run(async_main())
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
